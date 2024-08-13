4dcdf808ac38fa2a06956f3b721e4bdb97e47ed9
DONGWHA\D2301202 <sh.hwang@dongwha.com>
2024-08-13 15:59:14 +0900

    test

--- src/pages/customerkiosk/Mpark.vue
+++ src/pages/customerkiosk/Mpark.vue
@@ -24,7 +24,7 @@ const pinchHandler = ({ offset: [scale, angle] }) => {
 // Use `usePinch` with the correct target
 onMounted(() => {
   const p = usePinch(pinchHandler, {
-    domTarget: rootNode,
+    domTarget: demoBox.value,
     eventOptions: {
       passive: false
     },
